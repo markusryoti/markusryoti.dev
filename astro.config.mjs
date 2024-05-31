9abe743076163b93d233029de9b0a99aa35c86c6
Markus Ryöti <mryoti@gmail.com>
2024-05-31 21:11:14 +0300

    add posthog, disable vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ export default defineConfig({
 	integrations: [tailwind()],
 	output: 'server',
 	adapter: vercel({
-		webAnalytics: { enabled: true },
+		webAnalytics: { enabled: false },
 	}),
 });

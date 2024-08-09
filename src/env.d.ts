2566bb5b9b0baa993216cf015ecf5d873a2984c1
Markus Ryöti <mryoti@gmail.com>
2024-08-09 21:08:22 +0300

    maybe fix ts error

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,9 @@
 /// <reference path="../.astro/db-types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface Window {
+  posthog: {
+    opt_out_capturing(): void;
+  };
+}
